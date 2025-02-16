d7449c7a2b05524b04480222af76c2a111b77c54
mleibeng <mleibeng@student.42heilbronn.de>
2025-02-16 09:58:29 +0100

    Forgot to add to AppDataSource for initialization

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -6,17 +6,27 @@
 /*   By: mleibeng <mleibeng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 17:10:21 by mleibeng          #+#    #+#             */
-/*   Updated: 2025/02/16 07:40:22 by mleibeng         ###   ########.fr       */
+/*   Updated: 2025/02/16 09:57:46 by mleibeng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import { DataSource } from "typeorm";
 import { UserModel } from "./models/user.model";
+import { TournamentChatModel, TournamentMatchModel, TournamentModel, TournamentRoundModel, TournamentStatisticsModel } from "./models/tournament.model";
+import { GameModel } from "./models/game.model";
 
 export const AppDataSource = new DataSource ({
     type: "sqlite",
     database: 'database.sqlite',
-    entities: [UserModel],
+    entities: [
+        UserModel,
+        GameModel,
+        TournamentModel,
+        TournamentChatModel,
+        TournamentMatchModel,
+        TournamentRoundModel,
+        TournamentStatisticsModel,
+    ],
     synchronize: true, //set to false for production later!!
     logging: true
 })
